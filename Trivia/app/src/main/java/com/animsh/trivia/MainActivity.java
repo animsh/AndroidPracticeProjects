0040567b570ba83d7ec4274926442ed0cbab4ee0
animsh <animsh.more@gmail.com>
2020-12-13 16:31:50 +0530

    Setting Up Question Bank Class - Setup JsonArrayRequest Object

--- Trivia/app/src/main/java/com/animsh/trivia/MainActivity.java
+++ Trivia/app/src/main/java/com/animsh/trivia/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.animsh.trivia.data.QuestionBank;
+import com.animsh.trivia.model.Question;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new QuestionBank().getQuestions();
     }
 }
---
+++ Trivia/app/src/main/java/com/animsh/trivia/data/QuestionBank.java
@@ -0,0 +1,42 @@
+package com.animsh.trivia.data;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.animsh.trivia.controller.AppController;
+import com.animsh.trivia.model.Question;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionBank {
+
+    private ArrayList<Question> questionArrayList = new ArrayList<>();
+    private String url = "https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json";
+
+    public List<Question> getQuestions() {
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                url,
+                (JSONArray) null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("JSON ARRAY: ", "onResponse: " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+        return null;
+    }
+}
